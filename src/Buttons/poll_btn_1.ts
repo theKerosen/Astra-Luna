4888e8b4f445ae2bc1965af685286d5418d93f76
Lunixyz <otakinhuhdev@gmail.com>
2023-04-03 20:28:50 -0400

    Quick fix because my math is bad

--- src/Buttons/poll_btn_1.ts
+++ src/Buttons/poll_btn_1.ts
@@ -4,7 +4,7 @@ export async function execute(
   interaction: ButtonInteraction,
   client: XPManager
 ) {
-   if (
+ if (
     client.votes.has(
       `vote_${interaction.user.id}_${interaction.message.embeds[0].description}`
     )
@@ -12,13 +12,18 @@ export async function execute(
     return interaction.reply({
       content: "Você já votou.",
       ephemeral: true,
-    });
+    }); 
   client.votes.set(
     `vote_${interaction.user.id}_${interaction.message.embeds[0].description}`,
     true
   );
   const Embed = interaction.message.embeds[0];
-  Embed.fields[0].value = `${(parseInt(Embed.fields[0].value) + 1).toString()} (${(Math.ceil(parseInt((Embed.fields[0].value) + 1) / client.votes.size) * 100).toString()}%)`;
+  const votes1 = parseInt(Embed.fields[0].value) + 1
+  const votes2 = parseInt(Embed.fields[1].value)
+  const percentage1 = votes1 / (votes1+votes2) * 100
+  const percentage2 = votes2 / (votes1+votes2) * 100
+  Embed.fields[0].value = `${votes1} (${Math.floor(percentage1)}%)`;
+  Embed.fields[1].value = `${votes2} (${Math.floor(percentage2)}%)`;
   interaction.message.edit({ embeds: [Embed] });
   interaction.reply({
     content: "Obrigado por ter votado.",
--- src/Buttons/poll_btn_2.ts
+++ src/Buttons/poll_btn_2.ts
@@ -4,18 +4,23 @@ export async function execute(
   interaction: ButtonInteraction,
   client: XPManager
 ) {
-  if (
+    if (
     client.votes.has(
       `vote_${interaction.user.id}_${interaction.message.embeds[0].description}`
     )
   )
-    return interaction.reply({ content: "Você já votou.", ephemeral: true });
+    return interaction.reply({ content: "Você já votou.", ephemeral: true }); 
   client.votes.set(
     `vote_${interaction.user.id}_${interaction.message.embeds[0].description}`,
     true
   );
   const Embed = interaction.message.embeds[0];
-  Embed.fields[1].value = `${(parseInt(Embed.fields[1].value) + 1).toString()} (${((Math.ceil((parseInt(Embed.fields[1].value) + 1) / client.votes.size) * 100)).toString()}%)`;
+  const votes1 = parseInt(Embed.fields[0].value)
+  const votes2 = parseInt(Embed.fields[1].value) + 1
+  const percentage1 = votes1 / (votes1+votes2) * 100
+  const percentage2 = votes2 / (votes1+votes2) * 100
+  Embed.fields[0].value = `${votes1} (${Math.floor(percentage1)}%)`;
+  Embed.fields[1].value = `${votes2} (${Math.floor(percentage2)}%)`;
   interaction.message.edit({ embeds: [Embed] });
   interaction.reply({ content: "Obrigado por ter votado.", ephemeral: true });
 }
